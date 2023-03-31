caaf3e26124a3a643156eb65941bf66c3275ab61
Rubek Maharjan <rubekmhzn7@gmail.com>
2023-03-31 19:07:42 +0545

    delete ignore files

--- src/lib/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyByFShOcsr58iy6HyIDmVug2cRJI60zmuQ",
-  authDomain: "districts-of-nepal.firebaseapp.com",
-  projectId: "districts-of-nepal",
-  storageBucket: "districts-of-nepal.appspot.com",
-  messagingSenderId: "557788994043",
-  appId: "1:557788994043:web:e828556090e2bb75ca535e"
-};
