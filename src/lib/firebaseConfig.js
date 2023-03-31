d90640446efd72b1abd074041060028edf6cb86d
Rubek Maharjan <rubekmhzn7@gmail.com>
2023-03-31 19:13:31 +0545

    change

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByFShOcsr58iy6HyIDmVug2cRJI60zmuQ",
+  authDomain: "districts-of-nepal.firebaseapp.com",
+  projectId: "districts-of-nepal",
+  storageBucket: "districts-of-nepal.appspot.com",
+  messagingSenderId: "557788994043",
+  appId: "1:557788994043:web:e828556090e2bb75ca535e"
+};
